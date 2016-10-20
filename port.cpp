7464e241e7b7840d4b39fb83339b9747fed11188
Rafał Gębski <rafgeb@gmail.com>
2016-10-21 01:50:53 +0200

    Port class implemented

--- port.cpp
+++ port.cpp
@@ -16,12 +16,24 @@ Port8Bit::~Port8Bit(){
 }
 void Port8Bit::Write(uint8_t data){
 	
-	__asm__volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
+	__asm__ volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
 	
 }
 uint8_t Port8Bit::Read(){
+	uint8_t result;
+	__asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (portnumber));
+	return result;
+}
+
+Port8BitSlow::Port8BitSlow(int16_t portnumber)
+: Port8Bit(portnumber){
+}
+Port8BitSlow::~Port8BitSlow(){
+}
+void Port8BitSlow::Write(uint8_t data){
 	
-	__asm__volatile("outb %1, %0" : : "=a" (result), "Nd" (portnumber));
+	__asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (data), "Nd" (portnumber));
+	
 }
 
 Port16Bit::Port16Bit(int16_t portnumber)
@@ -31,12 +43,13 @@ Port16Bit::~Port16Bit(){
 }
 void Port16Bit::Write(uint16_t data){
 	
-	__asm__volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
+	__asm__ volatile("outw %0, %1" : : "a" (data), "Nd" (portnumber));
 	
 }
 uint16_t Port16Bit::Read(){
-	
-	__asm__volatile("outb %1, %0" : : "=a" (result), "Nd" (portnumber));
+	uint16_t result;
+	__asm__ volatile("inw %1, %0" : "=a" (result) : "Nd" (portnumber));
+	return result;
 }
 
 Port32Bit::Port32Bit(int16_t portnumber)
@@ -46,10 +59,11 @@ Port32Bit::~Port32Bit(){
 }
 void Port32Bit::Write(uint32_t data){
 	
-	__asm__volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
+	__asm__ volatile("outl %0, %1" : : "a" (data), "Nd" (portnumber));
 	
 }
 uint32_t Port32Bit::Read(){
-	
-	__asm__volatile("outb %1, %0" : : "=a" (result), "Nd" (portnumber));
+	uint32_t result;
+	__asm__ volatile("inl %1, %0" : "=a" (result) : "Nd" (portnumber));
+	return result;
 }
--- port.h
+++ port.h
@@ -9,41 +9,37 @@ class Port {
 protected:
 	int16_t portnumber;
 	Port(int16_t portnumber);
-	~Port():
+	~Port();
 	
 };
 
 class Port8Bit : public Port {
 public:
-	int16_t portnumber;
 	Port8Bit(int16_t portnumber);
-	~Port8Bit():
+	~Port8Bit();
 	virtual void Write(uint8_t data);
 	virtual uint8_t Read();
 };
 
 class Port8BitSlow : public Port8Bit {
 public:
-	int16_t portnumber;
 	Port8BitSlow(int16_t portnumber);
+	~Port8BitSlow();
 	virtual void Write(uint8_t data);
-	~Port8BitSlow():
 };
 
 class Port16Bit : public Port {
 public:
-	int16_t portnumber;
 	Port16Bit(int16_t portnumber);
-	~Port16Bit():
+	~Port16Bit();
 	virtual void Write(uint16_t data);
 	virtual uint16_t Read();
 };
 
 class Port32Bit : public Port {
 public:
-	int16_t portnumber;
 	Port32Bit(int16_t portnumber);
-	~Port32Bit():
+	~Port32Bit();
 	virtual void Write(uint32_t data);
 	virtual uint32_t Read();
 };
