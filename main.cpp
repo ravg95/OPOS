de805c9d51aca559268ebdac6afcfac48e99b380
vickyorlo <vickyorlo@gmail.com>
2016-10-21 00:19:40 +0200

    added necessary functions to ostream, scrolldown isnt necessarily working yet

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
-
-#include "types.h"
+#include "stddef.h"
+#include "stdint.h"
 #include "ostream.h"
 
 
-extern "C" void kmain(const void* multiboot_struct, uint32_t magic_num){
+void delay(int countdown_limit) {
+	volatile int w = 0;
+	while (w<countdown_limit) {
+		w++;
+	}
+}
+
+extern "C" void kmain(const void* multiboot_struct, uint32_t magic_num) {
 	ostream osout = ostream();
-	osout<<"posOS wita WAS!!";
-	
-	while(1);
+	for (int i = 0; i < 50; i++) {
+		i %= 127;
+		osout << "We are OS noobs: " << (char)i+'a' << '\n';
+		delay(100000);
+	}
+
+	//for (int i = 0;i < 10;i++) osout.WriteString("youve got some nerve coming out here\ninto the eye of the storm\nwith a noose around your neck\nobey dont think consume\n");
+
+	while (1);
 }
