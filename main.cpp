bf35cf667b1f626f92782fcd7f9ffb8f2a8b5d8c
Rafał Gębski <rafgeb@gmail.com>
2016-10-21 15:01:38 +0200

    Descriptive name, no OPOS

--- main.cpp
+++ main.cpp
@@ -9,16 +9,32 @@ void delay(int countdown_limit) {
 		w++;
 	}
 }
+void printHelloMessage(){
+ostream osout = ostream();
+
+osout<<"  ____                      __       __      _           __            \n";
+osout<<" / __ \\_  _____ _____    __/ /  ___ / /_ _  (_)__  ___ _/ /_ __        \n";
+osout<<"/ /_/ / |/ / -_) __/ |/|/ / _ \\/ -_) /  ' \\/ / _ \\/ _ `/ / // /        \n";
+osout<<"\\____/|___/\\__/_/  |__,__/_//_/\\__/_/_/_/_/_/_//_/\\_, /_/\\_, /         \n";
+osout<<"  / _ \\___  ___  ____                            /___/  /___/          \n";
+osout<<" / ___/ _ \\/ _ \\/ __/                                                  \n";
+osout<<"/_/___\\___/\\___/_/        __  _             ____         __            \n";
+osout<<" / __ \\___  ___ _______ _/ /_(_)__  ___ _  / __/_ _____ / /____ __ _   \n";
+osout<<"/ /_/ / _ \\/ -_) __/ _ `/ __/ / _ \\/ _ `/ _\\ \\/ // (_-</ __/ -_)  ' \\_ \n";
+osout<<"\\____/ .__/\\__/_/  \\_,_/\\__/_/_//_/\\_, / /___/\\_, /___/\\__/\\__/_/_/_(_)\n";
+osout<<"    /_/                           /___/      /___/                     \n";
+osout<<"\n";
+}
+
 
 extern "C" void kmain(const void* multiboot_struct, uint32_t magic_num) {
-	ostream osout = ostream();
-	for (int i = 0; i < 5000; i++) {
-		i %= 127;
-		osout << "We are OS noobs: " << (char)i+'a' << '\n';
-		delay(100000);
-	}
+	
+	
+	printHelloMessage();
 
 	//for (int i = 0;i < 10;i++) osout.WriteString("youve got some nerve coming out here\ninto the eye of the storm\nwith a noose around your neck\nobey dont think consume\n");
 
-	while (1);
+	while (1){
+		delay(10000);
+	};
 }
